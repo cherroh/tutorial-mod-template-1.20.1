ba85159b9d0c27a247c8abc5d8e3647b69e7ad5f
cherroh <155501029+cherroh@users.noreply.github.com>
2024-05-28 21:40:23 -0700

    Initial commit

---
+++ src/main/java/net/cherrow/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.cherrow.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
