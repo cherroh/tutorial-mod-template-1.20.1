6626210402a64b8397fb559b0e7c9c109666a163
cherroh <155501029+cherroh@users.noreply.github.com>
2024-05-31 17:39:31 -0700

    added block entity renderer

--- src/main/java/net/cherrow/tutorialmod/TutorialModClient.java
+++ src/main/java/net/cherrow/tutorialmod/TutorialModClient.java
@@ -1,18 +1,21 @@
 package net.cherrow.tutorialmod;
 
-import net.cherrow.tutorialmod.screen.GemPolishingScreen;
-import net.cherrow.tutorialmod.screen.ModScreenHandlers;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.cherrow.tutorialmod.block.ModBlocks;
+import net.cherrow.tutorialmod.block.entity.ModBlockEntities;
+import net.cherrow.tutorialmod.block.entity.renderer.GemPolishingBlockEntityRenderer;
 import net.cherrow.tutorialmod.entity.ModEntities;
 import net.cherrow.tutorialmod.entity.client.ModModelLayers;
 import net.cherrow.tutorialmod.entity.client.PorcupineModel;
 import net.cherrow.tutorialmod.entity.client.PorcupineRenderer;
+import net.cherrow.tutorialmod.screen.GemPolishingScreen;
+import net.cherrow.tutorialmod.screen.ModScreenHandlers;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class TutorialModClient implements ClientModInitializer {
     @Override
@@ -30,5 +33,7 @@ public class TutorialModClient implements ClientModInitializer {
         EntityModelLayerRegistry.registerModelLayer(ModModelLayers.PORCUPINE, PorcupineModel::getTexturedModelData);
 
         HandledScreens.register(ModScreenHandlers.GEM_POLISHING_SCREEN_HANDLER, GemPolishingScreen::new);
+
+        BlockEntityRendererFactories.register(ModBlockEntities.GEM_POLISHING_STATION_BLOCK_ENTITY, GemPolishingBlockEntityRenderer::new);
     }
 }
