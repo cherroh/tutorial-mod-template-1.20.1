1d2ae11796ebff3f06fda75b6226bbe07cf93723
cherroh <155501029+cherroh@users.noreply.github.com>
2024-06-01 22:01:00 -0700

    added chestnut tree generation

--- src/main/java/net/cherrow/tutorialmod/world/gen/ModWorldGeneration.java
+++ src/main/java/net/cherrow/tutorialmod/world/gen/ModWorldGeneration.java
@@ -3,5 +3,7 @@ package net.cherrow.tutorialmod.world.gen;
 public class ModWorldGeneration {
     public static void generateModWorldGen() {
         ModOreGeneration.generateOres();
+
+        ModTreeGeneration.generateTrees();
     }
 }
